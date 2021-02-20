4ee02df6f7466f724072e22bf5a6e7986d82b485
Evgeniy Ni <jounts@gmail.com>
2021-02-20 22:29:08 +0300

    added map app

---
+++ map/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views import View
+
+
+class IndexView(View):
+    def get(self, request):
+        context = {'layer_list': ['Anivskiy_ST_SViV:vs', 'Anivskiy_ST_SViV:TS']}
+
+        return render(request, 'map/index.html', context)
