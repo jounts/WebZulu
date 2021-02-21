08f5635513b7e334a0b1ceec2b94da33396feca6
Evgeniy Ni <jounts@gmail.com>
2021-02-21 13:18:23 +0300

    added layer to list

--- map/views.py
+++ map/views.py
@@ -5,6 +5,6 @@ from django.views import View
 
 class IndexView(View):
     def get(self, request):
-        context = {'layer_list': ['Anivskiy_ST_SViV:vs', 'Anivskiy_ST_SViV:TS']}
+        context = {'layer_list': ['Anivskiy_ST_SViV:vs', 'Anivskiy_ST_SViV:TS', 'Anivskiy_ST_SViV:vo']}
 
         return render(request, 'map/index.html', context)
