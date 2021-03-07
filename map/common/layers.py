2be9e924b291bfcadb8295cc0f586327fa2afe31
Evgeniy Ni <jounts@gmail.com>
2021-03-08 00:28:27 +0300

    updated layers.py

--- map/common/layers.py
+++ map/common/layers.py
@@ -1,3 +1,7 @@
+"""
+Module retrieving published projects and layers on zulu server
+"""
+
 from xml.etree import ElementTree
 
 import requests
@@ -5,11 +9,19 @@ import requests
 from WebZulu.settings import GIS_SERVER
 
 
-def get_layer_list():
+def get_layers() -> dict:
+    """
+    collect data to dict from zulu server
+    :return: dict
+    """
+    return parse_data(get_source_data())
 
-    layer_list = []
 
-    url = GIS_SERVER
+def get_source_data() -> str:
+    """
+    request GetLayerList from zulu server
+    :return: str
+    """
 
     xml = '''<?xml version="1.0" encoding="UTF-8"?>
         <zulu-server service='zws' version='1.0.0'>
@@ -18,14 +30,30 @@ def get_layer_list():
             </Command>
         </zulu-server>'''.encode('utf-8')
 
-    response = requests.post(url=url, data=xml)
+    response = requests.post(url=GIS_SERVER, data=xml)
 
-    root = ElementTree.fromstring(response.text)
+    return response.text
 
-    for elements in root[0]:
-        if elements.tag == 'Layer':
-            for element in elements:
-                if element.tag == 'Name':
-                    layer_list.append(element.tag)
 
-    return layer_list
+def parse_data(data: str) -> dict:
+    """
+    parse source data from zulu server
+    :param data: str
+    :return: dict
+    """
+    parsed_data = dict()
+
+    root = ElementTree.fromstring(data)
+
+    for element in root[0]:
+        if element.tag == 'Layer' and len(element):
+            project = element[0].text[:element[0].text.find(':')]
+            if project not in parsed_data.keys():
+                parsed_data[project] = []
+            parsed_data[project].append(
+                {'fullname': element[0].text,
+                 'name': element[0].text[element[0].text.find(':') + 1:],
+                 'title': element[1].text}
+            )
+
+    return parsed_data
