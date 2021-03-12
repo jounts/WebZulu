4645745d06e8857badda8fbd92b49ceec51c16ea
Evgeniy Ni <jounts@gmail.com>
2021-03-13 01:22:11 +0300

    models

--- map/models.py
+++ map/models.py
@@ -14,13 +14,19 @@ class Layer(models.Model):
     name = models.CharField(max_length=100)
     full_name = models.CharField(max_length=250)
     title = models.CharField(max_length=150)
+    namespace = models.ForeignKey('NameSpace', on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.name}'
 
 
 class User(models.Model):
-    auth_user = models.OneToOneField(UserAuth, on_delete=models.CASCADE)
+    auth_user = models.OneToOneField(UserAuth, on_delete=models.SET_NULL, null=True, blank=True)
 
     def _str__(self):
         return f'{UserAuth.Username}'
+
+
+class Project(models.Model):
+    namespace = models.ForeignKey('NameSpace', on_delete=models.CASCADE)
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
