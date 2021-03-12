dbc58f28f50bdba185b53bda2c17b79adedb7c67
Evgeniy Ni <jounts@gmail.com>
2021-03-13 00:54:50 +0300

    fix

--- map/models.py
+++ map/models.py
@@ -3,16 +3,16 @@ from django.contrib.auth.models import User as UserAuth
 
 
 class NameSpace(models.Model):
-    name = models.Charfield(max_length=150)
-    short_name = models.Charfield(max_length=50, blank=True)
+    name = models.CharField(max_length=150)
+    short_name = models.CharField(max_length=50, blank=True)
 
     def __str__(self):
         return f'{self.name}'
 
 class Layer(models.Model):
-    name = models.Charfield(max_length=100)
-    full_name = models.Charfield(max_length=250)
-    title = models.Charfield(max_length=150)
+    name = models.CharField(max_length=100)
+    full_name = models.CharField(max_length=250)
+    title = models.CharField(max_length=150)
 
     def __str__(self):
         return f'{self.name}'
@@ -22,5 +22,5 @@ class User(models.Model):
     auth_user = models.OneToOneField(UserAuth, on_delete=models.CASCADE)
     
     def _str__(self):
-        return f'{UserAuth.username}'
+        return f'{UserAuth.Username}'
         
