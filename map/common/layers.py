dbd37bcb657eb388d62627f3c6b5643f218797e1
Evgeniy Ni <jounts@gmail.com>
2021-03-12 21:00:21 +0300

    added zulu auth

--- map/common/layers.py
+++ map/common/layers.py
@@ -7,6 +7,7 @@ from xml.etree import ElementTree
 import requests
 
 from WebZulu.settings import GIS_SERVER
+from map.common.zulu_auth import get_credentials
 
 
 def get_layers() -> dict:
@@ -57,3 +58,65 @@ def parse_data(data: str) -> dict:
             )
 
     return parsed_data
+
+
+def get_source_bounds(full_layer_name: str) -> str:
+    """
+    Requests GetLayerBounds from zulu server
+    :param full_layer_name: str
+    :return: str
+    """
+    xml = f"""<zulu-server service="zws" version="1.0.0">
+<Command>
+<GetLayerBounds>
+<Layer>{full_layer_name}</Layer>
+</GetLayerBounds>
+</Command>
+</zulu-server>""".encode('utf-8')
+
+    headers = {'Authorization': 'Basic ' + get_credentials()}
+
+    response = requests.post(url=GIS_SERVER, headers=headers, data=xml)
+
+    return response.text
+
+
+def parse_bounds(source_data: str) -> dict:
+    """
+    Parsing layer bounds from xml string
+    :param source_data: str
+    :return: dict
+    """
+    parsed_bounds = dict()
+
+    root = ElementTree.fromstring(source_data)
+
+    for element in list(root[0][0]):
+        if element.attrib['CRS'] == 'EPSG:4326':
+            return element.attrib
+
+
+def get_centre_map(layer_list: list) -> tuple:
+    """
+
+    :param layer_list: list
+    :return: tuple (lon, lat)
+    """
+    lat_list = []
+    lon_list = []
+
+    for layer in layer_list:
+        bounds = parse_bounds(get_source_bounds(layer))
+
+        if bounds is not None:
+            lat_list.append((float(bounds['minx']) + float(bounds['maxx'])) / 2)
+            lon_list.append((float(bounds['miny']) + float(bounds['maxy'])) / 2)
+
+    if lat_list and lon_list:
+        return [round(sum(lon_list) / len(lon_list), 6), round(sum(lat_list) / len(lat_list), 6)]
+    else:
+        return [0, 0]
+
+
+if __name__ == '__main__':
+    print(get_centre_map(layer_list = ['test:vs', 'test:ts']))
--- map/views.py
+++ map/views.py
@@ -1,10 +1,26 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
+
+from WebZulu.settings import GIS_SERVER
+from .common.zulu_auth import get_credentials
+from .common.layers import get_centre_map
+
+
+class MapAdminView(View):
+    pass
 
 
 class IndexView(View):
     def get(self, request):
-        context = {'layer_list': ['Anivskiy_ST_SViV:vs', 'Anivskiy_ST_SViV:TS', 'Anivskiy_ST_SViV:vo']}
+        layers = ['Niznevartovsk_AST:Gaz', 'Niznevartovsk_AST:SanOchistka', 'Niznevartovsk_AST:Svet']# ['Anivskiy_ST_SViV:vs', 'Anivskiy_ST_SViV:vo', 'Anivskiy_ST_SViV:TS'] #['test:vs', 'test:ts']
+        context = {'layer_list': layers}
+        context.update({'credentials':get_credentials()})
+        context.update({'GIS_SERVER': GIS_SERVER})
+        context.update({'CENTRE_CORD': get_centre_map(layers)})
 
         return render(request, 'map/index.html', context)
+
+
+class PublicView(View):
+    def get(self, request):
+        pass
