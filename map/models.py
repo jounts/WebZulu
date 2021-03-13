64747c8146166a6d66e91199c33dca377804b278
Evgeniy Ni <jounts@gmail.com>
2021-03-14 01:57:12 +0300

    update models

--- map/models.py
+++ map/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User as UserAuth
+from django.contrib.auth.models import User
 
 
 class NameSpace(models.Model):
@@ -14,19 +14,15 @@ class Layer(models.Model):
     name = models.CharField(max_length=100)
     full_name = models.CharField(max_length=250)
     title = models.CharField(max_length=150)
-    namespace = models.ForeignKey('NameSpace', on_delete=models.CASCADE)
+    namespace = models.ForeignKey('NameSpace', on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
-        return f'{self.name}'
-
-
-class User(models.Model):
-    auth_user = models.OneToOneField(UserAuth, on_delete=models.SET_NULL, null=True, blank=True)
-
-    def _str__(self):
-        return f'{UserAuth.Username}'
+        return f'{self.full_name}'
 
 
 class Project(models.Model):
     namespace = models.ForeignKey('NameSpace', on_delete=models.CASCADE)
-    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.namespace}: {self.user}'
