3078a3b3fd321c9fd036a6ea575e0da06c8938e1
Evgeniy Ni <jounts@gmail.com>
2021-03-15 16:23:41 +0300

    updated urls

--- map/urls.py
+++ map/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 
-from .views import IndexView, MapIndexView, MapAdminView
+from .views import IndexView, MapIndexView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('map/', MapIndexView.as_view(), name='signed'),
-    path('mapadmin/', MapAdminView.as_view())
 ]
