49a40a1463e18966335bdf481bb5541e4b993e0e
Evgeniy Ni <jounts@gmail.com>
2021-03-16 12:49:18 +0300

    chaged func write_layers

--- map/tasks.py
+++ map/tasks.py
@@ -1,7 +1,7 @@
 from WebZulu.celery import app
-from .common.layers import parse_layers
+from .common.layers import write_layers
 
 
 @app.task
 def import_zulu_data_task():
-    parse_layers()
+    write_layers()
